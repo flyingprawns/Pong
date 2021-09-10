235a44a25223daac6f0eabb5485ff977f0b99ebd
Qijie Zhu <qzhu9@ucsc.edu>
2021-09-10 15:38:14 -0700

    created game screen

---
+++ main.py
@@ -0,0 +1,10 @@
+from turtle import Screen
+
+# Create game screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Pong!")
+
+# Exit on click
+screen.exitonclick()
