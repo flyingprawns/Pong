f811dc92ef6d9873815420b1d761531ab78f127e
Qijie Zhu <qzhu9@ucsc.edu>
2021-09-10 18:01:48 -0700

    added paddle bounce

--- main.py
+++ main.py
@@ -15,17 +15,17 @@ screen.title("Pong!")
 screen.tracer(0)
 
 # Create paddles and ball
-paddle_left = Paddle(PADDLE_LEFT_POSITION)
-paddle_right = Paddle(PADDLE_RIGHT_POSITION)
+left_paddle = Paddle(PADDLE_LEFT_POSITION)
+right_paddle = Paddle(PADDLE_RIGHT_POSITION)
 ball = Ball(BALL_POSITION)
 screen.update()
 
 # Listen for user input
 screen.listen()
-screen.onkey(key="w", fun=paddle_left.up)
-screen.onkey(key="s", fun=paddle_left.down)
-screen.onkey(key="Up", fun=paddle_right.up)
-screen.onkey(key="Down", fun=paddle_right.down)
+screen.onkey(key="w", fun=left_paddle.up)
+screen.onkey(key="s", fun=left_paddle.down)
+screen.onkey(key="Up", fun=right_paddle.up)
+screen.onkey(key="Down", fun=right_paddle.down)
 
 # Start game
 game_is_on = True
@@ -35,6 +35,10 @@ while game_is_on:
     # Detect collisions
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.wall_bounce()
+    # Detect paddle collision
+    if (ball.distance(right_paddle) < 80 and ball.xcor() > 320) or \
+            (ball.distance(left_paddle) < 80 and ball.xcor() < -310):
+        ball.paddle_bounce()
     # Update screen and wait 0.5 seconds
     screen.update()
     time.sleep(0.5)
